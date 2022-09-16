5780c0c9dedf32ba6441966c438fd3ffad7d7d92
Aneeza <amali78696@gmail.com>
2022-09-16 13:27:11 +0200

    ajout ul

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,6 +5,10 @@ function Banner() {
     return(
         <div className="banner">
             <img src={logo} alt="Groupomania" className="logo"/>
+            <ul className='log'>
+                <li className='el-log'>Inscription</li>
+                <li className='el-log'>Connexion</li>
+            </ul>
         </div>
     )
 }
